2e472b1a94e1bacf1f170cddd1855fd186c12c77
Artem Vernyhora <artemvernyhora8@icloud.com>
2025-11-06 23:58:25 +0200

    finish 02-hw

---
+++ src/controllers/notesController.js
@@ -0,0 +1,42 @@
+import { Note } from '../models/note.js';
+import createHttpError from 'http-errors';
+
+export const getAllNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.json(notes);
+};
+
+export const getNoteById = async (req, res, next) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.json(note);
+};
+
+export const createNote = async (req, res) => {
+  const result = await Note.create(req.body);
+
+  res.status(201).json(result);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const result = await Note.findByIdAndDelete(noteId);
+  if (!result) throw createHttpError(404, 'Note not found');
+
+  res.json(result);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+  const result = await Note.findByIdAndUpdate({ _id: noteId }, req.body, {
+    new: true,
+  });
+  if (!result) throw createHttpError(404, 'Note not found');
+
+  res.json(result);
+};
---
+++ src/db/connectMongoDB.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const { MONGO_URL } = process.env;
+
+export const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log('✅ MongoDB connection established successfully');
+  } catch (error) {
+    console.log(`Error connect database ${error.message}`);
+    throw error;
+  }
+};
