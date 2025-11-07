9380441736eae5aa2dee18d7910ae9e403dd9fa2
Artem Vernyhora <artemvernyhora8@icloud.com>
2025-11-08 01:03:31 +0200

    finish 03-hw

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import router from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -17,6 +18,8 @@ app.use(express.json());
 app.use('/notes', router);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
